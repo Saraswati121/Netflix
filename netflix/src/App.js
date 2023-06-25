0fac4f4193b8c930c015bb7a70d7a78cb494291f
Saraswati121 <99686266+Saraswati121@users.noreply.github.com>
2023-06-25 15:54:46 +0530

    complete landing page

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { BsGlobe } from 'react-icons/bs';
-
 function App() {
   return (
     <div className="App">
@@ -182,84 +186,122 @@ function App() {
         <h1 className="hero__title">Frequently Asked Questions</h1>
         <div className="FAQ__list">
           <div className="FAQ__accordian">
-            <button className="FAQ__title">
-              What is Netflix?<i className="plus"><BsGlobe/></i>
-            </button>
-            <div className="FAQ__visible">
-              <p>
-                Netflix is a streaming service that offers a wide variety of
-                award-winning TV shows, movies, anime, documentaries and more –
-                on thousands of internet-connected devices.
-              </p>
-              <p>
-                You can watch as much as you want, whenever you want, without a
+            <Accordion className='main'>
+            <AccordionSummary
+             expandIcon={<ExpandMoreIcon />}
+             aria-controls="panel1a-content"
+             id="panel1a-header"
+            >
+           <Typography style={{ fontSize: '20px' }}>What is Netflix?</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+            <Typography style={{ fontSize: '17px' , marginTop:"7px" }}>
+              Netflix is a streaming service that offers a wide variety of
+              award-winning TV shows, movies, anime, documentaries and more –
+              on thousands of internet-connected devices.
+              You can watch as much as you want, whenever you want, without a
                 single ad – all for one low monthly price. There's always
                 something new to discover, and new TV shows and movies are added
                 every week!
-              </p>
-            </div>
+            </Typography>
+           </AccordionDetails>
+           </Accordion>
           </div>
           <div className="FAQ__accordian">
-            <button className="FAQ__title">
-              How much does netflix cost?<i class="plus"><BsGlobe/></i>
-            </button>
-            <div className="FAQ__visible">
-              <p>
+          <Accordion>
+            <AccordionSummary
+             expandIcon={<ExpandMoreIcon />}
+             aria-controls="panel1a-content"
+             id="panel1a-header"
+            >
+           <Typography style={{ fontSize: '20px' }}>How much does netflix cost?</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+            <Typography style={{ fontSize: '17px' , marginTop:"7px" }}>
                 Watch Netflix on your smartphone, tablet, Smart TV, laptop, or
                 streaming device, all for one fixed monthly fee. Plans range
                 from ₹ 199 to ₹ 799 a month. No extra costs, no contracts.
-              </p>
-            </div>
+            </Typography>
+           </AccordionDetails>
+           </Accordion>
           </div>
           <div className="FAQ__accordian">
-            <button className="FAQ__title">
-              Where can i watch?<i className="plus"><BsGlobe/></i>
-            </button>
-            <div className="FAQ__visible">
-              <p>
-                Watch anywhere, anytime, on an unlimited number of devices. Sign
+          <Accordion>
+            <AccordionSummary
+             expandIcon={<ExpandMoreIcon />}
+             aria-controls="panel1a-content"
+             id="panel1a-header"
+            >
+           <Typography style={{ fontSize: '20px' }}>Where can i watch?</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+            <Typography style={{ fontSize: '17px' , marginTop:"7px" }}>
+              Watch anywhere, anytime, on an unlimited number of devices. Sign
                 in with your Netflix account to watch instantly on the web at
                 netflix.com from your personal computer or on any
                 internet-connected device that offers the Netflix app, including
                 smart TVs, smartphones, tablets, streaming media players and
                 game consoles.
-              </p>
-              <p>
+            </Typography>
+            <Typography style={{ fontSize: '17px' , marginTop:"5px" }}>
                 You can also download your favourite shows with the iOS,
                 Android, or Windows 10 app. Use downloads to watch while you're
                 on the go and without an internet connection. Take Netflix with
                 you anywhere.
-              </p>
-            </div>
+            </Typography>
+           </AccordionDetails>
+           </Accordion>
           </div>
           <div className="FAQ__accordian">
-            <button className="FAQ__title">
-              How do I cancel?<i className="plus"><BsGlobe/></i>
-            </button>
-            <div className="FAQ__visible">
-              <p>
-                Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.
-              </p>
-            </div>
+          <Accordion>
+            <AccordionSummary
+             expandIcon={<ExpandMoreIcon />}
+             aria-controls="panel1a-content"
+             id="panel1a-header"
+            >
+           <Typography style={{ fontSize: '20px' }}>How do I cancel?</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+            <Typography style={{ fontSize: '17px' , marginTop:"7px" }}>
+            Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.
+            </Typography>
+           </AccordionDetails>
+           </Accordion>
           </div>
           <div className="FAQ__accordian">
-            <button className="FAQ__title">
-              What can I watch from Netflix?<i className="plus"><BsGlobe/></i>
-            </button>
-            <div className="FAQ__visible">
-              <p>
-                Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.
-              </p>
-            </div>
+          <Accordion>
+            <AccordionSummary
+             expandIcon={<ExpandMoreIcon />}
+             aria-controls="panel1a-content"
+             id="panel1a-header"
+            >
+           <Typography style={{ fontSize: '20px' }}>What can I watch from Netflix?</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+            <Typography style={{ fontSize: '17px' , marginTop:"7px" }}>
+            Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.
+            </Typography>
+           </AccordionDetails>
+           </Accordion>
           </div>
           <div className="FAQ__accordian">
-            <button className="FAQ__title">
-              Is Netflix good for kids?<i className="plus"><BsGlobe/></i>
-            </button>
-            <div className="FAQ__visible">
-              <p>The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space.</p>
-              <p>Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.</p>
-            </div>
+          <Accordion>
+            <AccordionSummary
+             expandIcon={<ExpandMoreIcon />}
+             aria-controls="panel1a-content"
+             id="panel1a-header"
+            >
+           <Typography style={{ fontSize: '20px' }}>Is Netflix good for kids?</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+            <Typography style={{ fontSize: '17px' , marginTop:"7px" }}>
+            The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space.
+            </Typography>
+            <Typography style={{ fontSize: '17px' , marginTop:"5px" }}>
+            Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.
+            </Typography>
+           </AccordionDetails>
+           </Accordion>
           </div>
         </div>
         <div className="FAQ__get__started__email">
